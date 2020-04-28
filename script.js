be89761943c2603e9e718083ea72de0721fb927e
Bob <bob@bobs-mbp.mynet>
2020-04-28 22:30:25 +0100

    working carousel

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
-var slideIndex=0;
-
-function plusSlides(n){
-    showSlides(slideIndex+=n)
+// lets hold a slide position for each container
+var indexes = {
+    aboutme: 0,
+    paint: 0,
+    travel: 0,
 }
 
-function currentSlide(n){
-    showSlides(slideIndex=n);
+function plusSlides(n, id){
+    indexes[id] += n
+    showSlides()
 }
 
-function showSlides(n){
-    var i;
+function currentSlide(n, id){
+    indexes[id] = n
+    showSlides()
+}
 
-
-    var slides=document.getElementsByClassName('mySlides');
-    if (slides.length===0) return
-    var dots=document.getElementsByClassName('dot');
-
-
-    if (n>slides.length){slideIndex=1}
-    if (n<1){slideIndex=slides.length}
-    for (i=0;i<slides.length;i++){
-        slides[i].style.display='none';
+function showSlides(){
+    // find an array (list) of all of the slideshow containers on the page
+    var listOfContainers = document.getElementsByClassName('slideshow-container')
+    // if there are no containers, we can stop here! :)
+    if (listOfContainers.length === 0) {
+        return
     }
-    for (i=0;i<dots.length;i++){
-        dots[i].className=dots[i].className.replace('active','');
+    // loop through each of the found slideshow containers in the list
+    for (var i = 0; i < listOfContainers.length; i++) {
+        // first, we need to grab the slideshow container element from the list above using the index (i)
+        var slideshowContainer = listOfContainers[i]
+        // then we will save the id to a variable for later use
+        var id = slideshowContainer.id
+        // next we will find all of the mySlide elements inside the current slideshow container
+        var slides = slideshowContainer.getElementsByClassName('mySlides')
+        // if there are no slides, log a warning and skip to the next slideshow container
+        if (slides.length === 0) {
+            console.warn('you have no slides!')
+            continue
+        }
+        // check that n doesnt exceed the amount of slides
+        if (indexes[id] > slides.length - 1) {
+            indexes[id] = 0
+        }
+        // check that n doesnt go below 0
+        if (indexes[id] < 0) {
+            indexes[id] = slides.length - 1
+        }
+        // next we will find all of the dot elements inside the current slideshow container
+        var dots = slideshowContainer.getElementsByClassName('dot')
+        // if there are no dots found, log a warning an skip to the next container
+        if (dots.length === 0) {
+            console.warn('you have no dots!')
+            continue
+        }
+        // loop through each slide and set to display none
+        for(var u = 0; u < slides.length; u++) {
+            // grab the slide element using the index (u)
+            var slide = slides[u]
+            // make invisible (display: none)
+            slide.style.display = 'none'
+        }
+        // loop through each dot and remove the 'active' class
+        for(var d = 0; d < dots.length; d++) {
+            // grab the dot element using the index (d)
+            var dot = dots[d]
+            // update the classname to the current classname WITHOUT the word 'active'
+            dot.className = dot.className.replace('active', '')
+        }
+        // make the slide visible
+        slides[indexes[id]].style.display = 'block'
+        // set 'active' on the dot
+        dots[indexes[id]].className += " active"
     }
-    slides[slideIndex-1].style.display='block';
-    dots[slideIndex-1].className+=' active';
 }
+
 function onPageLoad(){
     highlightCurrentLink()
-    showSlides(0)
+    showSlides()
     accordion()
 }
 window.onload=onPageLoad

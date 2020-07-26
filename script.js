0ec0f9a67cfdf29c6950b47eae0cf675c948ef1d
Lucile <lucile.vacquie@gmail.com>
2020-07-26 19:06:09 +0100

    loads

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var indexes = {
     aboutme: 0,
     paint: 0,
     travel: 0,
+    coding: 0,
 }
 
 function plusSlides(n, id){
@@ -79,8 +80,19 @@ function onPageLoad(){
 window.onload=onPageLoad
 
 function highlightCurrentLink(){
-    var path=document.URL.substring(document.URL.lastIndexOf('/')+1)
-    var currentLinks=document.querySelectorAll('a[href="'+path+'"]')
+    //first we need to get the current address (eg: http://lucile.com/aboutme.html)
+    var currentURL=document.URL
+    //then we need to find the position of the last occurence of /
+    //this will return us the position of the '/' in '/aboutme.html'
+    //then we want to +1 to index so that we get the position of the 'a' in 'aboutme.html'
+    var index=currentURL.lastIndexOf('/')+1
+    //next we pass the index to substring() to return the value 'aboutme.html'
+    //substring returns the part of the string AFTER the index we pass in
+    //eg http://lucile.com/   => aboutme.html <= this last part is a sustring
+    var path=currentURL.substring(index)
+    //now we can find all 'a' element that have the href value 'aboutme.html'
+    var currentLinks = document.querySelectorAll('a[href="'+path+'"]')
+    //highlight the 'a' element containing the href value 'aboutme.html'
     currentLinks.forEach(function(link){
         link.className += ' highlight-link'
     })
